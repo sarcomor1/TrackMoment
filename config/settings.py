cf8f2f29efd84d537eaaf31fd2a8b5a5e3cc85d5
sam <sarcomor@gmail.com>
2025-07-14 13:32:21 +0330

    added models

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     #local
     'trackmoment',
     'account',
+    'data',
 ]
 
 MIDDLEWARE = [
--- data/models.py
+++ data/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from enum import Enum
 
-class user(models.Model):
+class EnumIntegration(Enum):
+    LIBRARY = 'football'
+
+class EnumActivety(Enum):
+    SHOPPING = 'shopping'
+
+
+class User(models.Model):
     id = models.BigAutoField(primary_key= True)
     email = models.EmailField()
     password = models.CharField(max_length= 225)
@@ -9,20 +17,23 @@ class user(models.Model):
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
     last_login = models.DateTimeField()
+    integration = models.ForeignKey('integration', on_delete= models.CASCADE)
+    activity = models.ForeignKey('activity', on_delete= models.CASCADE)
+    
 
-class user(models.Model):
+class Integration(models.Model):
     integration_id = models.BigAutoField(primary_key= True)
     user_id = models.BigIntegerField()
-    integration_type = models.enums()
+    integration_type = models.CharField(max_length= 20, choices=[(tag.value, tag.name) for tag in EnumIntegration])
     integration_details = models.JSONField()
     last_sync = models.DateTimeField()
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
 
-class user(models.Model):
+class Activity(models.Model):
     activity_id = models.BigAutoField(primary_key= True)
     user_id = models.BigIntegerField()
-    activity_type = models.enums()
+    activity_type = models.CharField(max_length= 20, choices=[(tag.value, tag.name) for tag in EnumActivety])
     activity_details = models.JSONField()
     image = models.JSONField()
     location = models.TextField()
